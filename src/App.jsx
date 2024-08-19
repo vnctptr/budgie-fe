93a37e75d9d1ee9fb408f54cc0383c6afb1b44a7
Vincent Potrykus <vapotrykus@gmail.com>
2024-08-18 20:25:27 -0700

    Add router and Login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
 import "./App.css";
-import Sidebar from "./components/Sidebar/Sidebar.jsx";
+import Login from "./pages/Login/Login.jsx";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className="flex">
-        <Sidebar />
-        <div>
-          <h1 className="text-3xl font-bold underline">Hello world!</h1>
-          <div className="card bg-primary text-primary-content w-96">
-            <div className="card-body">
-              <h2 className="card-title">Card title!</h2>
-              <p>If a dog chews shoes whose shoes does he choose?</p>
-              <div className="card-actions justify-end">
-                <button className="btn">Buy Now</button>
-              </div>
-            </div>
-          </div>
-
-          <div>
-            <a href="https://vitejs.dev" target="_blank">
-              <img src={viteLogo} className="logo" alt="Vite logo" />
-            </a>
-            <a href="https://react.dev" target="_blank">
-              <img src={reactLogo} className="logo react" alt="React logo" />
-            </a>
-          </div>
-          <h1>Vite + React</h1>
-
-          <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is {count}
-            </button>
-            <p>
-              Edit <code>src/App.jsx</code> and save to test HMR
-            </p>
-          </div>
-          <p className="read-the-docs">
-            Click on the Vite and React logos to learn more
-          </p>
-        </div>
-      </div>
+      <BrowserRouter>
+        <Route exact path="/" component={Login} />
+        <Route path="/login" component={Login} />
+        <Login />
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,9 @@
+const Login = () => {
+  return (
+    <>
+      <div className="artboard phone-1">320×568</div>
+    </>
+  );
+};
+
+export default Login;
