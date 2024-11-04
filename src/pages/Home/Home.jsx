5c15836b6351e8b06a903426b8e886683f36f6a4
Vincent Potrykus <vapotrykus@gmail.com>
2024-11-03 17:43:37 -0800

    Add scrollable component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,8 +5,74 @@ const Home = () => {
     <>
       <div className="flex">
         <Sidebar />
-        <div>
-          <h1 className="text-3xl font-bold m-5">Welcome to Budgie!</h1>
+        <div className="w-full flex flex-col h-dvh">
+          <div className="w-full h-56">
+            <h4 className="font-bold m-5">Nov 2024</h4>
+          </div>
+          <div className="p-2 overflow-y-auto">
+            <ul>
+              <li>Groceries: $150</li>
+              <li>Dining Out: $120</li>
+              <li>Gasoline: $80</li>
+              <li>Entertainment: $90</li>
+              <li>Rent: $1200</li>
+              <li>Utilities: $150</li>
+              <li>Internet: $60</li>
+              <li>Phone: $70</li>
+              <li>Insurance: $200</li>
+              <li>Clothing: $100</li>
+              <li>Health & Wellness: $75</li>
+              <li>Transportation: $50</li>
+              <li>Personal Care: $45</li>
+              <li>Subscriptions: $30</li>
+              <li>Gifts: $85</li>
+              <li>Education: $200</li>
+              <li>Travel: $300</li>
+              <li>Home Supplies: $40</li>
+              <li>Savings: $500</li>
+              <li>Emergency Fund: $150</li>
+              <li>Pets: $60</li>
+              <li>Charity: $70</li>
+              <li>Loan Payments: $400</li>
+              <li>Credit Card: $150</li>
+              <li>Gym Membership: $25</li>
+              <li>Streaming Services: $20</li>
+              <li>Hobbies: $60</li>
+              <li>Work Lunches: $45</li>
+              <li>Books: $30</li>
+              <li>Cooking Utensils: $30</li>
+              <li>Groceries: $150</li>
+              <li>Dining Out: $120</li>
+              <li>Gasoline: $80</li>
+              <li>Entertainment: $90</li>
+              <li>Rent: $1200</li>
+              <li>Utilities: $150</li>
+              <li>Internet: $60</li>
+              <li>Phone: $70</li>
+              <li>Insurance: $200</li>
+              <li>Clothing: $100</li>
+              <li>Health & Wellness: $75</li>
+              <li>Transportation: $50</li>
+              <li>Personal Care: $45</li>
+              <li>Subscriptions: $30</li>
+              <li>Gifts: $85</li>
+              <li>Education: $200</li>
+              <li>Travel: $300</li>
+              <li>Home Supplies: $40</li>
+              <li>Savings: $500</li>
+              <li>Emergency Fund: $150</li>
+              <li>Pets: $60</li>
+              <li>Charity: $70</li>
+              <li>Loan Payments: $400</li>
+              <li>Credit Card: $150</li>
+              <li>Gym Membership: $25</li>
+              <li>Streaming Services: $20</li>
+              <li>Hobbies: $60</li>
+              <li>Work Lunches: $45</li>
+              <li>Books: $30</li>
+              <li>Cooking Utensils: $30</li>
+            </ul>
+          </div>
         </div>
       </div>
     </>
