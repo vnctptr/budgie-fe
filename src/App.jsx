6432a0330518ba629d9f8eedd282cfc8dc0b7e65
Vincent Potrykus <vapotrykus@gmail.com>
2024-11-03 17:43:14 -0800

    Clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-// import { useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "/vite.svg";
 import "./App.css";
 import Login from "./pages/Login/Login.jsx";
 import Home from "./pages/Home/Home.jsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  // const [count, setCount] = useState(0);
-
   return (
     <>
       <BrowserRouter>
